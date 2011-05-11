0bc73687e05f485776aae3bac12f925fd14d6054
joshaguilar <joshaguilar@media3creative.com>
2011-05-11 16:21:50 -0500

    Worked on Gallery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -39,3 +39,46 @@ $(window).load(function() {
   });
   
 });
+
+
+
+$(document).ready(function(){
+
+//Larger thumbnail preview 
+
+$("ul.thumb li").hover(function() {
+	$(this).css({'z-index' : '10'});
+	$(this).find('img').addClass("hover").stop()
+		.animate({
+			marginTop: '-110px', 
+			marginLeft: '-110px', 
+			top: '50%', 
+			left: '50%', 
+			width: '150px', 
+			height: '150px',
+			padding: '20px' 
+		}, 200);
+	
+	} , function() {
+	$(this).css({'z-index' : '0'});
+	$(this).find('img').removeClass("hover").stop()
+		.animate({
+			marginTop: '0', 
+			marginLeft: '0',
+			top: '0', 
+			left: '0', 
+			width: '100px', 
+			height: '100px', 
+			padding: '5px'
+		}, 400);
+});
+
+//Swap Image on Click
+	$("ul.thumb li a").click(function() {
+		
+		var mainImage = $(this).attr("href"); //Find Image Name
+		$("#main_view img").attr({ src: mainImage });
+		return false;		
+	});
+ 
+});
